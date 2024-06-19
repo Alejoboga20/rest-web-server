ee8fc4a46574b2a2e6e2e291fe1decf6d53b0594
Alejoboga20 <alejoboga19@gmail.com>
2024-06-19 15:51:41 -0300

    serve json and html files

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
+import { readFileSync } from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 
-	res.write('Hello World');
-	res.end();
+	// res.writeHead(200, { 'Content-Type': 'text/html' });
+	// res.write(`<h1>Hello URL: ${req.url}</h1>`);
+	// res.end();
+	// const data = { name: 'John Doe', age: 25, city: 'New York' };
+	// res.writeHead(200, { 'Content-Type': 'application/json' });
+	// res.write(JSON.stringify(data));
+	// res.end();
+	if (req.url === '/') {
+		const htmlFile = readFileSync('./public/index.html', 'utf-8');
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.write(htmlFile);
+		res.end();
+	}
 });
 
 server.listen(3000, () => console.log('server running on port 3000'));
