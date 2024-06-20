4e2a02b54bb7b43df35046f038562d151af687a1
Alejoboga20 <alejoboga19@gmail.com>
2024-06-20 15:41:05 -0300

    find todo by id

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,12 +1,22 @@
 import { Request, Response } from 'express';
 
+const todos = [
+	{ id: 1, name: 'Do laundry', completed: false, createdAt: new Date() },
+	{ id: 2, name: 'Buy groceries', completed: false, createdAt: new Date() },
+	{ id: 3, name: 'Clean bathroom', completed: false, createdAt: new Date() },
+];
+
 export class TodosController {
 	constructor() {}
 
 	public getTodos = (req: Request, res: Response) => {
-		res.json([
-			{ id: 1, name: 'Do laundry', completed: false },
-			{ id: 2, name: 'Buy groceries', completed: false },
-		]);
+		res.json(todos);
+	};
+
+	public getTodoById = (req: Request, res: Response) => {
+		const todoId = parseInt(req.params.id);
+		const todo = todos.find((todo) => todo.id === todoId);
+
+		todo ? res.json(todo) : res.status(404).json({ message: 'Todo not found' });
 	};
 }
