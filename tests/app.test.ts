b0897c720682726a6ab8ce1204bbe35289ac9d9f
Alejoboga20 <alejoboga19@gmail.com>
2024-07-01 11:10:50 -0300

    test app

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,5 +1,18 @@
+import { envs } from '../src/config/envs';
+import { Server } from '../src/presentation/server';
+
+jest.mock('../src/presentation/server');
+
 describe('App Tests', () => {
-	test('should work', () => {
-		expect(true).toBe(true);
+	test('should call server with arguments', async () => {
+		await import('../src/app');
+
+		expect(Server).toHaveBeenCalledTimes(1);
+		expect(Server).toHaveBeenCalledWith({
+			port: envs.PORT,
+			publicPath: envs.PUBLIC_PATH,
+			routes: expect.any(Function),
+		});
+		expect(Server.prototype.start).toHaveBeenCalledTimes(1);
 	});
 });
